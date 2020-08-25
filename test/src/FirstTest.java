dc739515ffcbbe2b278b11210a8f8e21dfda2fb5
liangxihua <1466380831@qq.com>
2020-08-25 17:59:13 +0800

    测试创建项目，并且成功创建项目关联了本地与远程仓库

---
+++ test/src/FirstTest.java
@@ -0,0 +1,12 @@
+/**
+ * Description:不在能知，而在能行
+ *
+ * @author: 梁西华
+ * @date: 2020/8/25/17:54
+ **/
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("这就是一次尝试，接下来回持续性完成算法题");
+    }
+}
