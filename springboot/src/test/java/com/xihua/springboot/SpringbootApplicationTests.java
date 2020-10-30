c45795353923173821b980e1eb0b683d67d0e3b5
liangxihua <1466380831@qq.com>
2020-10-30 10:38:03 +0800

    springboot缓存cache
    Signed-off-by: lk <lk@tapfun.me>

---
+++ springboot/src/test/java/com/xihua/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xihua.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
